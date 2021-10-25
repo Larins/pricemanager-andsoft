cc5eae3c7958435dd51464d0322501de4650d4d3
Lara Saiz <69240139+Larins@users.noreply.github.com>
2021-10-25 23:23:35 +0200

    update sql

--- queries.sql
+++ queries.sql
@@ -71,3 +71,119 @@ WHERE NOT EXISTS (
         AND delay_days =  10
         AND price = 100
 ) LIMIT 1;
+
+
+SELECT article, original_price, discount, increment, original_price*discount*increment AS final_price, delay_days, prom_date_ini, prom_date_fin 
+FROM
+(SELECT 
+	A.name AS article, 
+    A.price AS original_price, 
+    CASE 
+    	WHEN MIN(P.discount) > 1 THEN 1
+        ELSE MIN(P.discount)
+    END AS discount,
+    CASE 
+    	WHEN MAX(P.discount) < 1 THEN 1
+        ELSE MAX(P.discount)
+    END AS increment,
+    A.delay_days AS delay_days, 
+    P.date_ini AS prom_date_ini, 
+    P.date_fin AS prom_date_fin
+FROM articles A
+LEFT JOIN promos P ON P.delay_days = A.delay_days  OR P.delay_days IS NULL
+WHERE A.name = '24h'
+AND P.date_ini BETWEEN '2021-12-01' AND '2021-12-31' 
+ OR P.date_fin BETWEEN '2021-12-01' AND '2021-12-31'
+GROUP BY article, original_price, delay_days, prom_date_ini, prom_date_fin) AS Z
+;
+
+
+
+SELECT
+    CASE 
+    	WHEN MIN(P.discount) > 1 THEN 1
+        ELSE MIN(P.discount)
+    END AS discount,
+    CASE 
+    	WHEN MAX(P.discount) < 1 THEN 1
+        ELSE MAX(P.discount)
+    END AS increment,
+    P.delay_days AS delay_days, 
+    P.date_ini AS prom_date_ini, 
+    P.date_fin AS prom_date_fin
+FROM promos P
+WHERE P.date_ini BETWEEN '2021-12-01' AND '2021-12-31' 
+OR P.date_fin BETWEEN '2021-12-01' AND '2021-12-31'
+;
+
+
+SELECT A.id, A.name AS article, A.origin, A.destination, A.delay_days, A.price
+FROM articles A
+WHERE A.name = '24h';
+
+
+
+SELECT
+    CASE 
+    	WHEN MIN(P.discount) > 1 THEN 1
+        ELSE MIN(P.discount)
+    END AS discount,
+    CASE 
+    	WHEN MAX(P.discount) < 1 THEN 1
+        ELSE MAX(P.discount)
+    END AS increment,
+    P.delay_days AS delay_days, 
+    P.date_ini AS prom_date_ini, 
+    P.date_fin AS prom_date_fin
+FROM promos P
+WHERE P.date_ini BETWEEN '2021-12-01' AND '2021-12-31' 
+OR P.date_fin BETWEEN '2021-12-01' AND '2021-12-31'
+;
+
+
+SELECT A.id, A.name AS article, A.origin, A.destination, A.delay_days AS delay_article, Z.delay_days AS delay_promo, A.price, Z.discount, Z.increment
+FROM
+(SELECT
+    CASE 
+    	WHEN MIN(P.discount) > 1 THEN 1
+        ELSE MIN(P.discount)
+    END AS discount,
+    CASE 
+    	WHEN MAX(P.discount) < 1 THEN 1
+        ELSE MAX(P.discount)
+    END AS increment,
+    P.delay_days AS delay_days, 
+    P.date_ini AS prom_date_ini, 
+    P.date_fin AS prom_date_fin
+FROM promos P
+WHERE P.date_ini BETWEEN '2021-12-01' AND '2021-12-31' 
+OR P.date_fin BETWEEN '2021-12-01' AND '2021-12-31') AS Z
+FROM articles A
+LEFT JOIN Z ON Z.delay_days = A.delay_days OR Z.delay_days IS NULL
+WHERE A.name = '24h';
+
+
+SELECT A.id, A.name AS chosen_article, '2021-12-22' AS chosen_date, A.origin, A.destination, A.delay_days AS delay_article, Z.delay_days AS delay_promo, A.price AS initial_price, Z.discount, Z.increment, A.price*Z.discount*Z.increment AS final_price
+FROM articles A
+LEFT JOIN 
+    (
+        SELECT
+            CASE 
+                WHEN MIN(P.discount) > 1 THEN 1
+                ELSE MIN(P.discount)
+            END AS discount,
+            CASE 
+                WHEN MAX(P.discount) < 1 THEN 1
+                ELSE MAX(P.discount)
+            END AS increment,
+            P.delay_days AS delay_days,
+            P.origin, 
+            P.destination
+        FROM promos P
+        WHERE P.date_ini <= '2021-12-22' AND P.date_fin >= '2021-12-22'
+    ) AS Z ON (Z.delay_days = A.delay_days OR Z.delay_days IS NULL) AND A.origin = Z.origin AND A.destination = Z.destination
+WHERE A.name = '24h'
+AND A.origin = 'bcn'
+AND A.destination = 'bcn'
+AND A.delay_days = 1
+;
--- schema.sql
+++ schema.sql
@@ -8,9 +8,7 @@ CREATE TABLE articles (
 	name VARCHAR(100),
 	origin VARCHAR(100),
     destination VARCHAR(100),
-	date_ini DATE,
     delay_days NUMERIC(65, 2),
-    date_fin DATE,
     price NUMERIC(65, 2)
 );
 DROP TABLE IF EXISTS promos;
@@ -26,23 +24,23 @@ CREATE TABLE promos (
     discount NUMERIC(65, 2)
 );
 
-INSERT INTO articles (name, origin, destination, date_ini, delay_days, price) VALUES 
-('24h', 'bcn', 'bcn', '2021-05-05', 1, 100),
-('RAPIDA', 'bcn', 'bcn', '2021-08-10', 10, 100),
-('NORMAL', 'bcn', 'bcn', '2021-10-24', 10, 100),
-('24h', 'bcn', 'bcn', '2021-12-25', 1, 100)
+INSERT INTO articles (name, origin, destination, delay_days, price) VALUES 
+('24h', 'bcn', 'bcn', 1, 500),
+('RAPIDA', 'bcn', 'bcn', 10, 200),
+('NORMAL', 'bcn', 'bcn', 10, 100),
+('24h', 'bcn', 'tgn', 1, 120),
+('RAPIDA', 'bcn', 'tgn', 10, 240),
+('NORMAL', 'bcn', 'tgn', 10, 580)
 
 ;
 
-UPDATE articles
-	SET date_fin = DATE_ADD(date_ini, INTERVAL delay_days DAY)
-;
 
 INSERT  INTO promos (name, origin, destination, date_ini, duration_days, delay_days, date_fin, discount) VALUES
 ('CumpleEmpresa', 'bcn', 'bcn', '2021-05-05', 1, 1, null,  0.8),
 ('Clearance 2021-08-01 to 2021-08-31', 'bcn', 'bcn', '2021-08-01', null, null, '2021-08-31', 0.5),
 ('Halloween', 'bcn', 'bcn', '2021-10-31', 1, 10, null, 1.2),
-('Xmas 2021-12-20 to 2022-01-10', 'bcn', 'bcn', '2021-12-20', null, null, '2022-01-10', 1.2)
+('Xmas 2021-12-20 to 2022-01-10', 'bcn', 'bcn', '2021-12-20', null, null, '2022-01-10', 1.2),
+('Lotería de Navidas', 'bcn', 'bcn', '2021-12-22', 1, 1, null,  0.95)
 ;
 
 UPDATE promos
